0def42d19aa362cd8270a7d4a6a7389122bba68d
odmin <dizz74@mail.ru>
2021-11-16 23:01:50 +0500

    init

---
+++ Part1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Part1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("calc by dizz");
+
+            try
+            {
+                Console.Write("Введите X:");
+                int x = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Введите Y:");
+                int y = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Введите код операции:");
+
+                Console.WriteLine("1:+");
+                Console.WriteLine("2:-");
+                Console.WriteLine("3:*");
+                Console.WriteLine("4:/");
+
+                int cmd = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Ваш выбор {0}",cmd);
+                int res;
+                switch (cmd) {
+                    case 1:
+                        res = x + y;
+                        break;
+                    case 2:
+                        res = x - y;
+                        break;
+                    case 3:
+                        res = x * y;
+                        break;
+                    case 4:
+                        res = x / y;
+                        break;
+                    default:
+                        throw new Exception("Нет операции с указанным номером"); 
+
+                }
+                Console.WriteLine("Результат: {0}",res);
+
+            }
+
+            catch (DivideByZeroException) {
+                Console.WriteLine("олло, ну делить на ноль совсем кринж");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Ошибка!" + e.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
